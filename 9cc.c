2abceefeb3d06f81165d8786181562a23ba0fc95
yamamto117@gmail.com <ymt@DESKTOP-JTVUS36.localdomain>
2020-04-19 13:11:41 +0900

    単項演算子（プラスとマイナス）を扱えるように変更した

--- 9cc.c
+++ 9cc.c
@@ -151,6 +151,7 @@ Node *new_node_num(int val){
 // プロトタイプ宣言
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr(){
@@ -167,16 +168,24 @@ Node *expr(){
 }
 
 Node *mul(){
-	Node *node = primary();
+	Node *node = unary();
 
 	for(;;){
 		if(consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if(consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
+}
+
+Node *unary(){
+	if(consume('+'))
+		return primary();
+	if(consume('-'))
+		return new_node(ND_SUB, new_node_num(0), unary());
+	return primary();
 }
 
 Node *primary(){
