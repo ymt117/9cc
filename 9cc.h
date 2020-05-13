c63ece87632e17d6ed605246aa8596c971fb86c4
yamamto117@gmail.com <ymt@DESKTOP-JTVUS36.localdomain>
2020-05-13 22:02:44 +0900

    ファイル分割をした

---
+++ 9cc.h
@@ -0,0 +1,66 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 入力プログラム
+char *user_input;
+
+// トークンの種類
+typedef enum{
+	TK_RESERVED,	// 記号
+	TK_NUM,			// 整数トークン
+	TK_EOF,			// 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token{
+	TokenKind kind;	// トークンの型
+	Token *next;	// 次の入力トークン
+	int val;		// kindがTK_NUMの場合，その数値
+	char *str;		// トークン文字列
+	int len;		// トークンの長さ
+};
+
+// 現在着目しているトークン
+Token *token;
+
+// 抽象構文木のノードの種類
+typedef enum{
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_EQ,	// ==
+	ND_NE,	// !=
+	ND_LT,	// <
+	ND_LE,	// <=
+	ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node{
+	NodeKind kind;	// ノードの型
+	Node *lhs;	// 左辺
+	Node *rhs;	// 右辺
+	int val;	// kindがND_NUMの場合のみ使う
+};
+
+// プロトタイプ宣言
+Token *tokenize();
+
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+void gen(Node *node);
---
+++ main.c
@@ -0,0 +1,28 @@
+#include "9cc.h"
+
+int main(int argc, char **argv){
+	if(argc != 2){
+		perror("引数の個数が正しくありません");
+		return 1;
+	}
+
+
+	// トークナイズしてパースする
+	user_input = argv[1];
+	token = tokenize(user_input);
+	Node *node = expr();
+
+	// アセンブリの前半部分を出力
+	printf(".intel_syntax noprefix\n");
+	printf(".global main\n");
+	printf("main:\n");
+
+	// 抽象構文木を下りながらコード生成
+	gen(node);
+
+	// スタックトップに式全体の値が残っているはずなので
+	// それをRAXにロードして関数からの返り値とする
+	printf("  pop rax\n");
+	printf("  ret\n");
+	return 0;
+}
